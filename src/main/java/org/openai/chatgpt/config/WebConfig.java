be901cbaacddfaba5a5d46ebb040a5e9ae6afd69
fredericdasilva <52976188+fredericdasilva@users.noreply.github.com>
2023-02-09 01:14:32 +0100

    :construction: fix(fix): CORS
    enables CORS requests from any origin to any endpoint v2

--- src/main/java/org/openai/chatgpt/config/WebConfig.java
+++ src/main/java/org/openai/chatgpt/config/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig  implements WebMvcConfigurer   {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**").allowedMethods("*");
 //                .allowedMethods("GET", "POST", "PUT", "DELETE")
 //                .allowedOrigins(myAllowedApi,"http://192.168.1.178:3000");
 
